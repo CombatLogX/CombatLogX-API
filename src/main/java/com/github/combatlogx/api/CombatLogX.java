ed857a7b1bd4e55dad9c854e2a559bd7243f52f8
SirBlobman <blobmansrealm@gmail.com>
2026-05-16 19:19:29 -0400

    Add annotations and UntagReason class

--- src/main/java/com/github/combatlogx/api/CombatLogX.java
+++ src/main/java/com/github/combatlogx/api/CombatLogX.java
@@ -1,12 +1,16 @@
 package com.github.combatlogx.api;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.github.combatlogx.api.expansion.ExpansionManager;
 import com.github.combatlogx.api.player.PlayerManager;
 import com.github.combatlogx.api.tag.TagManager;
+import com.github.combatlogx.api.timer.TimerManager;
 
 public interface CombatLogX {
     void reloadConfiguration();
-    ExpansionManager getExpansionManager();
-    PlayerManager getDataManager();
-    TagManager getTagManager();
+    @NotNull ExpansionManager getExpansionManager();
+    @NotNull PlayerManager getDataManager();
+    @NotNull TagManager getTagManager();
+    @NotNull TimerManager getTimerManager();
 }
---
+++ src/main/java/com/github/combatlogx/api/tag/UntagReason.java
@@ -0,0 +1,5 @@
+package com.github.combatlogx.api.tag;
+
+public enum UntagReason {
+    EXPIRE, SELF_DEATH, ENEMY_DEATH, ENEMY_FORGIVE, QUIT, KICK
+}
