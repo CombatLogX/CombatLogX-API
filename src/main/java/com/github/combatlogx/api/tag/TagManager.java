48447e56a69a024c0525c0913b027431b99fcb30
SirBlobman <blobmansrealm@gmail.com>
2026-05-16 19:30:46 -0400

    Fix method 'untag'

--- src/main/java/com/github/combatlogx/api/tag/TagManager.java
+++ src/main/java/com/github/combatlogx/api/tag/TagManager.java
@@ -8,7 +8,7 @@ import com.github.combatlogx.api.player.CombatPlayer;
 
 public interface TagManager {
     boolean addTag(@NotNull CombatPlayer player, @NotNull CombatTag tag);
-    void untag(@NotNull CombatPlayer player, @NotNull CombatTag tag);
+    void untag(@NotNull CombatPlayer player, @NotNull CombatTag tag, @NotNull UntagReason reason);
     boolean isTagged(@NotNull CombatPlayer player);
     @NotNull List<CombatTag> getTags(@NotNull CombatPlayer player);
     @NotNull List<CombatPlayer> getTaggedPlayers();
